eb53d52aae0dc2d318ebf86d196fa9603cff7664
christinew2 <christinewei2@gmail.com>
2021-09-28 16:54:05 -0500

    create navbar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css"
+
+
+const NavBar = () => {
+    return (
+        <nav className="nav-bar">
+    
+        </nav>
+    )
+}
---
+++ src/pages/App/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom'
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Christine Wei's Portfolio
+      </header>
+    </div>
+  );
+}
+
+export default App;
