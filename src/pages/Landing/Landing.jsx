89d612a7e31e66c2e68f2f2d067896d424067f76
christinew2 <christinewei2@gmail.com>
2021-09-29 19:13:24 -0500

    add Lottie animation to landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './Landing.css'
+import Animation from '../../components/misc/Animation'
+import code from '../../assets/animations/animation.json'
 
 const Landing = () => {
     return (
+        <>
+
         <h1>LANDING PAGE</h1>
+        <div className="animation-container">
+            <Animation animationData={code}></Animation>
+        </div>
+        </>
     );
 }
  
