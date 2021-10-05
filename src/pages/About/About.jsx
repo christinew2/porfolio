024951d810e34de5eceea7bb5baf5e3dd274638c
christinew2 <christinewei2@gmail.com>
2021-10-05 15:37:49 -0500

    about page styling

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,14 +4,29 @@ import './About.css'
 import email from '../../assets/icons/email.png'
 import GHLogo from '../../assets/icons/GitHub-Mark-64px.png'
 import LILogo from '../../assets/icons/LI-In-Bug.png'
+import christine from '../../assets/images/christine.jpeg'
 
 const About = () => {
     return (
-        <>
-        
-        <h1>About</h1>
-
-        </>
+        <section className="about-me">
+            <div className="about-me-img" >
+                <img src={christine} alt="christine"/>
+            </div>
+            <div className="about-me-content">
+                <p>I am a full stack developer with a passion for connecting with others and building tools and services that have lasting impact on my community, particularly in the healthcare industry. My background in biomedical engineering not only refined my ability to quickly tackle complex problems through systematic approaches, but also equipped me for cross-functional, multi-disciplinary roles. I love independent work, and especially thrive in team settings that challenge me and give me opportunities to explore different creative solutions.</p>
+                <div className="links">
+                    <a href="christinewei2@gmail.com">
+                        <img src={email} alt="email" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/christinew2/">
+                        <img src={LILogo} alt="linkedin" />
+                    </a>
+                    <a href="https://github.com/christinew2">
+                        <img src={GHLogo} alt="github" />
+                    </a>
+                </div>
+            </div>
+        </section>
     );
 }
  
